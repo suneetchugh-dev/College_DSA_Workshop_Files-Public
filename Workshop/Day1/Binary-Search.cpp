3f73529df83ea37ae6dc9d7736176257186aa3a7
suneetchugh-dev <valorise078gramming@gmail.com>
2026-07-01 11:44:53 +0530

    learnt how to implement infinite array search using linear approach

--- Workshop/Day1/Binary-Search.cpp
+++ Workshop/Day1/Binary-Search.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int key, int n)
-{
-    
+int binarySearch(int arr[], int key, int n) {
+    int start = 0;
+    int end = n - 1;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (key == arr[mid]) {
+            return mid;
+        } else if (key < arr[mid]) {
+            end = mid - 1;
+        } else if (key > arr[mid]) {
+            start = mid + 1;
+        } else {
+            cout << "The Searched Item Is Not Present in The Array";
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 14, 18, 22};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int index = binarySearch(arr, 18, n);
+    if(index==-1)
+    {
+        cout << "Index of the searched element Not Found, the element doesn't exist in the array" << endl;
+    }
+    else
+    {
+        cout << "Index of the searched element: " << index << endl;
+    }
 }
